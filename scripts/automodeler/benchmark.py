bdb14b8d98ace47b3fa8bbee25f917109bdad4d7
Hiroaki-violine <kryou649@gmail.com>
2023-08-07 19:56:29 +0900

    initial commit

---
+++ scripts/automodeler/benchmark.py
@@ -0,0 +1,7 @@
+def visualize_model(coordinate_1, coordinate_2):
+    for i in coordinate_1:
+        print(' '.join([str(j) for j in i]))
+    for i in coordinate_2:
+        print(' '.join([str(j) for j in i]))
+
+    return
